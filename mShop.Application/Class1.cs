fbba9c8c9232840bcfc9841118c573fba518d4dc
Tong Huy <anhtong8x@gmail.com>
2021-09-01 20:06:27 +0700

    create structure project

---
+++ mShop.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace mShop.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ mShop.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace mShop.Data
+{
+    public class Class1
+    {
+    }
+}

46b283107ba06f5b1debff8eb138eeadc178d4cd
Tong Huy <anhtong8x@gmail.com>
2021-09-10 21:54:53 +0700

    Finish lesson 9

---
+++ mShop.Data/EF/MShopDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace mShop.Data.EF
+{
+    public class mShopDbContextFactory : IDesignTimeDbContextFactory<MShopDbContext>
+    {
+        public MShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("")
+                .Build();
+
+            // chuoi conection string lay tu file appsetting.json
+
+            var connectionString = configuration.GetConnectionString("mShopDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<MShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new MShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
